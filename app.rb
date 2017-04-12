77b7edacdfd54e9b19a5de6efd3b4d56e77e2434
Vincent Martorano <martoranov94@gmail.com>
2017-04-12 15:00:59 -0400

    added email form

--- app.rb
+++ app.rb
@@ -15,3 +15,33 @@ end
 get "/contact" do
   erb :contact
 end
+
+post "/form-response" do
+  params.inspect
+
+  require 'sendgrid-ruby'
+
+  # set the from, subject and to addresses
+  from = SendGrid::Email.new(email: params[:from])
+  subject = params[:subject]
+  to = SendGrid::Email.new(email: params[:to])
+
+  # set the content to send in the email
+  content = SendGrid::Content.new(type: 'text/plain', value: params[:content])
+
+  # set the mail attribute values
+  mail = SendGrid::Mail.new(from, subject, to, content)
+
+  # pass in the sendgrid api key
+  sg = SendGrid::API.new(api_key: ENV["SENDGRID_API_KEY"])
+
+  # send the email
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+  # display the response status code and body
+  puts response.status_code
+  puts response.body
+
+  redirect "/contact"
+  #you can use these params as input to an email form (subject, from to etc)
+end
