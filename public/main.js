61359ca4b008c0b17284d27b312ee630157952c9
Vincent Martorano <martoranov94@gmail.com>
2017-04-12 17:22:43 -0400

    styling to home

--- public/main.js
+++ public/main.js
@@ -1,27 +1,75 @@
-// Modal
-var open = document.getElementById('modal');
-var exitButton = document.querySelector('.exit-button')
+// // Modal
+// var open = document.getElementById('modal');
+// var exitButton = document.querySelector('.exit-button')
+//
+// var timing = function(){
+//   open.setAttribute('style', 'display:block');
+// };
+//
+// setTimeout(timing, 1500);
+//
+// exitButton.addEventListener("click", function(){
+//  open.setAttribute('style', 'display:none');
+//  open.style.display = 'none'
+// });
+//
+//
+// document.setAttribute('style', 'display:block');
+//
+// function checkModal() {
+//  var email = document.querySelector("#email").value;
+//
+//  if (true) {
+//    alert("Benvenuti!");
+//  } else {
+//    alert("Error!")
+//  }
+// };
 
-var timing = function(){
-  open.setAttribute('style', 'display:block');
-};
 
-setTimeout(timing, 1500);
+// gallery
+// 
+// function replace(rome,venice,florence)
+// {
+//    rome = document.getElementById("rome-div");
+//    venice = document.getElementById("venice-div");
+//    florence = document.getElementById("florence-div");
+//    if( venice.style.display == "none")
+//    {
+//       d1.style.display = "none";
+//       d2.style.display = "block";
+//    }
+//    else
+//    {
+//       d1.style.display = "block";
+//       d2.style.display = "none";
+//    }
+// }
 
-exitButton.addEventListener("click", function(){
- open.setAttribute('style', 'display:none');
- open.style.display = 'none'
-});
 
 
-document.setAttribute('style', 'display:block');
 
-function checkModal() {
- var email = document.querySelector("#email").value;
 
- if (true) {
-   alert("Benvenuti!");
- } else {
-   alert("Error!")
- }
-};
+// $(document).ready(function(){
+//
+//   $('#venice-div').hide();
+//   $('#florence-div').hide();
+//
+//   $('#florence-link').click(function(){
+//       $('#rome-div').hide();
+//       $('#venice-div').hide();
+//       $('#florence-div').show();
+//   });
+//
+//   $('#venice-link').click(function(){
+//       $('#rome-div').hide();
+//       $('#florence-div').hide();
+//       $('#venice-div').show();
+//   });
+//
+//   $('#rome-link').click(function(){
+//       $('#venice-div').hide();
+//       $('#florence-div').hide();
+//       $('#rome-div').show();
+//   });
+// });
