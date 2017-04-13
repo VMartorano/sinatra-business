1e2ea8887d5ebdb2d8d6485bd15c17649b4f05c4
Vincent Martorano <martoranov94@gmail.com>
2017-04-13 11:31:09 -0400

    changed up gallery format, added js for it

--- public/main.js
+++ public/main.js
@@ -1,75 +1,10 @@
-// // Modal
-// var open = document.getElementById('modal');
-// var exitButton = document.querySelector('.exit-button')
-//
-// var timing = function(){
-//   open.setAttribute('style', 'display:block');
-// };
-//
-// setTimeout(timing, 1500);
-//
-// exitButton.addEventListener("click", function(){
-//  open.setAttribute('style', 'display:none');
-//  open.style.display = 'none'
-// });
-//
-//
-// document.setAttribute('style', 'display:block');
-//
-// function checkModal() {
-//  var email = document.querySelector("#email").value;
-//
-//  if (true) {
-//    alert("Benvenuti!");
-//  } else {
-//    alert("Error!")
-//  }
-// };
-
-
 // gallery
-// 
-// function replace(rome,venice,florence)
-// {
-//    rome = document.getElementById("rome-div");
-//    venice = document.getElementById("venice-div");
-//    florence = document.getElementById("florence-div");
-//    if( venice.style.display == "none")
-//    {
-//       d1.style.display = "none";
-//       d2.style.display = "block";
-//    }
-//    else
-//    {
-//       d1.style.display = "block";
-//       d2.style.display = "none";
-//    }
-// }
+var slides = document.querySelectorAll('.carousel .city');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,5000);
 
-
-
-
-
-// $(document).ready(function(){
-//
-//   $('#venice-div').hide();
-//   $('#florence-div').hide();
-//
-//   $('#florence-link').click(function(){
-//       $('#rome-div').hide();
-//       $('#venice-div').hide();
-//       $('#florence-div').show();
-//   });
-//
-//   $('#venice-link').click(function(){
-//       $('#rome-div').hide();
-//       $('#florence-div').hide();
-//       $('#venice-div').show();
-//   });
-//
-//   $('#rome-link').click(function(){
-//       $('#venice-div').hide();
-//       $('#florence-div').hide();
-//       $('#rome-div').show();
-//   });
-// });
+function nextSlide(){
+    slides[currentSlide].className = 'city';
+    currentSlide = (currentSlide+1)%city.length;
+    slides[currentSlide].className = 'city-showing';
+}
