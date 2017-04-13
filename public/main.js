6beb1ff42c02cf871f32a692f04df3bba9d42381
Vincent Martorano <martoranov94@gmail.com>
2017-04-13 14:31:24 -0400

    no conflicts, everything functions, need to finish styling

--- public/main.js
+++ public/main.js
@@ -10,8 +10,12 @@ var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,3000);
 
+
+
 function nextSlide(){
     goToSlide(currentSlide+1);
+
+}
 
 function previousSlide(){
     goToSlide(currentSlide-1);
